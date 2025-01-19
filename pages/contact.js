5093be81013fb568d8cfd48265a21d24fd917921
Сергей <UrbaSO@mail.ru>
2025-01-19 17:29:03 +0300

    Update contact.js

--- pages/contact.js
+++ pages/contact.js
@@ -1,7 +1,7 @@
 export default function Contact() {
     return (
         <div>
-            <h1>Свяжитесь с нами</h1>
+            <h1>Свяжитесь с ними</h1>
             <p>Адрес: ул. Новая, д. 10</p>
             <p>Телефон: +7 (999) 999-99-99</p>
             <p>Email: new-email@example.com</p>
